d59d497363a1e7a0c6ad1c99b18365762f90c42e
skwabrynmawr <skwa@brynmawr.edu>
2024-06-05 21:53:22 +0000

    Jun 5, 2024, 9:53 PM

--- demos/1/script.js
+++ demos/1/script.js
@@ -1,36 +1,13 @@
 function setup() {
   createCanvas(400, 400);
   background(255);
-  noFill();
-  stroke(0);
+  fill (200, 0, 22);
+  circle (200,200,100);
+  fill (40,75, 200)
+  circle(100,100,100)
+  fill (204,204,255)
+  square (50,50,25)
+  fill (200,15,150)
+  circle (250,250,20)
   
-  // Draw hand-drawn circle
-  beginShape();
-  vertex(100, 100);
-  vertex(110, 90);
-  vertex(120, 95);
-  vertex(130, 100);
-  vertex(140, 115);
-  vertex(130, 130);
-  vertex(120, 135);
-  vertex(110, 130);
-  vertex(100, 115);
-  endShape(CLOSE);
-  
-  // Draw hand-drawn square
-  beginShape();
-  vertex(200, 100);
-  vertex(250, 100);
-  vertex(255, 110);
-  vertex(250, 150);
-  vertex(200, 150);
-  vertex(195, 140);
-  endShape(CLOSE);
-  
-  // Draw hand-drawn triangle
-  beginShape();
-  vertex(150, 250);
-  vertex(200, 300);
-  vertex(100, 300);
-  endShape(CLOSE);
 }
